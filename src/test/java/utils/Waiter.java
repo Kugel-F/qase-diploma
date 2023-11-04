297c7d0eacc70226d29595b3ddf86a5416a521c2
Kugel-F <frantsavichDE@gmail.com>
2023-11-04 22:07:00 +0100

    Add new method for list Webelements

--- src/test/java/utils/Waiter.java
+++ src/test/java/utils/Waiter.java
@@ -8,23 +8,30 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
 public class Waiter {
 
-    public final static int WAIT_2_SECONDS = 2;
     public final static int WAIT_5_SECONDS = 5;
+    public final static int WAIT_10_SECONDS = 10;
 
     public static WebElement waitElementToBeClickable(WebElement element) {
 
-        return new WebDriverWait(DriverSingleton.getInstance().getDriver(), Duration.ofSeconds(WAIT_5_SECONDS))
+        return new WebDriverWait(DriverSingleton.getInstance().getDriver(), Duration.ofSeconds(WAIT_10_SECONDS))
                 .until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public static boolean waitListElementsInvisibleOf(List<WebElement> elements) {
+
+        return new WebDriverWait(DriverSingleton.getInstance().getDriver(), Duration.ofSeconds(WAIT_5_SECONDS))
+                .until(ExpectedConditions.invisibilityOfAllElements(elements));
     }
 
     public static WebElement waitVisibleOf(WebElement element) {
 
-        return new WebDriverWait(DriverSingleton.getInstance().getDriver(), Duration.ofSeconds(WAIT_5_SECONDS))
+        return new WebDriverWait(DriverSingleton.getInstance().getDriver(), Duration.ofSeconds(WAIT_10_SECONDS))
                 .until(ExpectedConditions.visibilityOf(element));
     }
 }
