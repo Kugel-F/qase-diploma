25b1ddc06c776e2588f6fd839ff2e112b538285e
Kugel-F <frantsavichDE@gmail.com>
2023-11-05 11:06:25 +0100

    Add new method 'deleteTestProject()'

--- src/test/java/tests/base/BaseTest.java
+++ src/test/java/tests/base/BaseTest.java
@@ -1,9 +1,12 @@
 package tests.base;
 
 import driver.DriverSingleton;
+import jdk.jfr.Description;
 import lombok.extern.log4j.Log4j2;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Listeners;
+import service.CurrentProjectPageService;
+import service.ProjectsServicePage;
 
 @Listeners(TestListener.class)
 @Log4j2
@@ -11,8 +14,16 @@ public class BaseTest {
 
     @AfterMethod(alwaysRun = true)
     public void closeBrowser() {
-
-        DriverSingleton.getInstance().closeDriver();
         log.info("Close browser");
+        DriverSingleton.getInstance().closeDriver();
+    }
+
+    @AfterMethod(onlyForGroups = {"Create new project"})
+    @Description("Delete created test project")
+    public void deleteTestProject() {
+        CurrentProjectPageService currentProjectPageService = new CurrentProjectPageService();
+        currentProjectPageService.openProjectsPage();
+        ProjectsServicePage projectsServicePage = new ProjectsServicePage();
+        projectsServicePage.removeProject(projectsServicePage.getProjectTitle());
     }
 }
