dcebe93a4cddc9784ca6f6fa2758b01675775b5f
Kugel-F <frantsavichDE@gmail.com>
2023-11-10 00:32:27 +0100

    add Constant.class with api constants

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -2,9 +2,19 @@ package utils;
 
 public class Constants {
 
+//    UI constants
     public static final String EMPTY_FIELD = "";
     public static final String REQUIREMENT_FULFILL_MESSAGE = "This field is required";
     public static final String INVALID_DATA_MESSAGE = "These credentials do not match our records.";
     public static final String CONFIRM = "CONFIRM";
+    //    API constants
+    public static final String TOKEN_NAME = "Token";
+    public static final String CONTENT_TYPE = "Content-type";
+    public static final String JSON = "application/json";
+//    int constants
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
     public static final int TWO = 2;
+    public static final int THREE = 3;
+
 }
